04cb86e5e1ceec4b1b24ee25fe33d766d2adcb88
Kipras Melnikovas <kipras@kipras.org>
2024-12-03 17:15:44 +0200

    implement note list with test data

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
+import { useState } from "react";
 import { css } from "emotion";
 
 import "./reset.css";
 
-import { Note } from "./ui/Note";
+import { NoteData } from "./ui/Note";
+import { NoteList } from "./ui/NoteList";
 
 function App() {
+	const [notesData, setNotesData] = useState<NoteData[]>([
+		{ id: 1, paragraphs: [""], title: "" }, //
+		{ id: 2, paragraphs: [""], title: "" }, //
+		{ id: 3, paragraphs: [""], title: "" }, //
+		{ id: 4, paragraphs: [""], title: "" }, //
+	]);
+
 	return (
 		<div className={styles.app}>
-			<Note />
+			<NoteList data={notesData} />
 		</div>
 	);
 }

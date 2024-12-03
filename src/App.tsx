726cafeb450d38fb4747b82f6d32b6b8966a841d
Kipras Melnikovas <kipras@kipras.org>
2024-12-03 17:47:38 +0200

    add simple search

--- src/App.tsx
+++ src/App.tsx
@@ -9,20 +9,48 @@ import { NoteList } from "./ui/NoteList";
 function App() {
 	const [notesData, setNotesData] = useState<NoteData[]>([
 		{ id: 1, paragraphs: [""], title: "" }, //
-		{ id: 2, paragraphs: [""], title: "" }, //
-		{ id: 3, paragraphs: [""], title: "" }, //
-		{ id: 4, paragraphs: [""], title: "" }, //
+		{ id: 2, paragraphs: ["lorem ipsum", "dolor", "sit", "amet"], title: "" }, //
+		{ id: 3, paragraphs: ["foo", "bar", "baz"], title: "fizzbuzz" }, //
+		{ id: 4, paragraphs: ["1", "2", "3"], title: "123" }, //
 	]);
+
+	const [search, setSearch] = useState("");
 
 	return (
 		<div className={styles.app}>
-			<NoteList data={notesData} />
+			<nav className={styles.nav.nav}>
+				<ul className={styles.nav.list}>
+					<li>
+						<input
+							placeholder="Search in notes.."
+							value={search}
+							onChange={(e) => setSearch(e.target.value)}
+						/>
+					</li>
+					<li className={styles.nav.userItem}>User</li>
+				</ul>
+			</nav>
+
+			<NoteList data={notesData} search={search} />
 		</div>
 	);
 }
 
 const styles = {
-	app: css``,
+	app: css`
+		margin: 0 0.5rem;
+	`,
+	nav: {
+		nav: css`
+			margin: 1rem 0 2rem 0;
+		`,
+		list: css`
+			display: flex;
+		`,
+		userItem: css`
+			margin-left: auto;
+		`,
+	},
 };
 
 export default App;
--- src/ui/NoteList.tsx
+++ src/ui/NoteList.tsx
@@ -1,19 +1,21 @@
-import { FC, useState } from "react";
+import { FC, useMemo, useState } from "react";
 
 import { css } from "emotion";
 import { Note, NoteData } from "./Note";
 
 export type NoteListProps = {
 	data: NoteData[];
+	search?: string;
 };
 
-export const NoteList: FC<NoteListProps> = ({ data }) => {
-	const [activeNote, setActiveNote] = useState(0);
+export const NoteList: FC<NoteListProps> = ({ data, search = "" }) => {
+	const searchedData: NoteData[] = useMemo(() => searchNotes(data, search), [data, search]);
+	const [activeNote, setActiveNote] = useState(-1);
 
 	return (
 		<div>
 			<ul id="note-list" className={styles.noteList}>
-				{data.map((x, i) => (
+				{searchedData.map((x, i) => (
 					<li
 						key={x.id}
 						onClick={() => {
@@ -32,5 +34,19 @@ const styles = {
 	noteList: css`
 		display: flex;
 		flex-direction: column;
+
+		& > * + * {
+			margin-top: 1rem;
+		}
 	`,
 };
+
+function searchNotes(notesData: NoteData[], search: string): NoteData[] {
+	return !search
+		? notesData
+		: notesData.filter(
+				(note) =>
+					note.title.includes(search) || //
+					note.paragraphs.some((p) => p.includes(search))
+			);
+}

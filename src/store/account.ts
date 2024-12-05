764c5a55295134c8f9aff63e5f78bc05ddd2c865
Kipras Melnikovas <kipras@kipras.org>
2024-12-05 13:07:49 +0200

    generate unique session id

--- src/store/account.ts
+++ src/store/account.ts
@@ -3,5 +3,12 @@ export const LS_SESSION_KEY = "note.kipras.org:session";
 export const ACCOUNT = {
 	session:
 		localStorage.getItem(LS_SESSION_KEY) ||
-		(localStorage.setItem(LS_SESSION_KEY, "1733247526774"), localStorage.getItem(LS_SESSION_KEY)!),
+		(localStorage.setItem(LS_SESSION_KEY, generateSessionID()), localStorage.getItem(LS_SESSION_KEY)!),
 } satisfies { session: string };
+
+function generateSessionID(): string {
+	const time: number = new Date().getTime();
+	const rand: number = Math.floor(Math.random() * 1e15);
+
+	return `${time}:${rand}`;
+}
